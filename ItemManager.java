3d984c9dcf6dd4f543c84d4bbebd86e8c48ff57e
co0obs <jacobolmilla@gmail.com>
2025-12-03 00:06:56 +0800

    added Remove Item functionality

--- ItemManager.java
+++ ItemManager.java
@@ -16,6 +16,14 @@ public class ItemManager {
     }
 
     public void viewItems() {
-        System.out.println("View feature not yet implemented.");
+        System.out.println("\n--- Current Items ---");
+
+        if (items.isEmpty()){
+            System.out.println("No items in the inventory.");
+        } else {
+            for (int i = 0; i < items.size(); i++){
+                System.out.println((i + 1) + ". " + items.get(i));
+            }
+        }
     }
 }
